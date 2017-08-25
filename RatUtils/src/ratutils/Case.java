83375989adf006d173621322d7f644037cf509fe
Alexander H <hehe3301@gmail.com>
2017-08-24 21:05:56 -0400

    Schell( XD ) of a Case class

--- RatUtils/src/ratutils/Case.java
+++ RatUtils/src/ratutils/Case.java
@@ -7,17 +7,18 @@ public class Case
     private String case_id;
     private String client;
     private String system;
+    private String distance;
     private String platform;
     private Boolean code_red;
     private String language;
-    private List<String> assigned_rate;
+    private List<String> assigned_rats;
 
     private Boolean fr; //Friend Request
-    private Boolean wr; //Wing Request
-    private Boolean prep; //Prep status
     private Boolean sysconf; //System confirmed status
-    private Boolean sys; //Rat to report if in system
+    private Boolean wr; //Wing Request
     private Boolean bc; //Rat to report if bacon is toasty
+    private Boolean prep; //Prep status
+    private Boolean sys; //Rat to report if in system
     private Boolean inst; //Rat to report if in instance with client
 
 }
